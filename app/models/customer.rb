a993afa6fbe4d5a9aa4941fd94abb5682473c981
NaomichiIzumi <bell.tsaeb.shoji@gmail.com>
2025-10-26 00:04:42 +0900

    add: カスタマーモデルにバリデーションとEnum設定を記載しました

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,13 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :status, presence: true
+
+  enum :status, {
+    normal: 0, # 通常
+    withdrawn: 1, # 退会済
+    banned: 2 # 停止
+  }
 end
