aa58d9bf8253af181e9e966af913c9525967af74
Omi354 <n.izumi@aby-tech.co.jp>
2025-10-27 22:19:49 +0900

    fix: デバイスControllerでストロングパラメーターへの追加が認識されないバグを修正しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :customers, controller: {
     sessions: "customers/sessions",
-    registration: "customers/registrations"
+    registrations: "customers/registrations"
   }
   devise_for :admins, controller: {
     sessions: "admins/sessions"
