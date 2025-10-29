725d3b3d2f75be032ea142aeb44752478c5bb918
Omi354 <n.izumi@aby-tech.co.jp>
2025-10-29 09:50:38 +0900

    feat: 顧客向けの商品一覧・詳細画面を作成しました

---
+++ app/controllers/customers/customer_base_controller.rb
@@ -0,0 +1,2 @@
+class Customers::CustomerBaseController < ApplicationController
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -15,4 +15,7 @@ class Product < ApplicationRecord
     ],
     message: "全く同じ商品名・商品説明・金額のセットが他にも存在します"
   }
+
+  scope :price_high_to_low, -> { order(price: :desc) }
+  scope :price_low_to_high, -> { order(price: :asc) }
 end
